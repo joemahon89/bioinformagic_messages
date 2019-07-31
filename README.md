9d7bf529d70475985b0898b4d0cbf3a754b4c072
jmahon <joseph.mahon@nhs.net>
2019-07-31 12:47:16 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# bioinformagic_messages
+# Bioinformagic Messages
+
+Generate completely useless bioinformatics-related loading messages.
+
+#### Usage
+```python
+import bioinformagic_messages as bm
+
+message = bm.Message()
+
+for i in range(0,10):
+	print(message.random_message())
+```
+#### Output
+```python
+"Inspecting algorithms"
+"Shaking domains"
+"Hand-crafting the cloud"
+"Splurging 00010111001"
+"Generating nucleotides"
+"Shaking URLs"
+"Synthesizing AI"
+"Parsing URLs"
+"Reticulating extensions"
+"Downloading sequences"
+```
---
+++ bioinformagic_messages.py
@@ -0,0 +1,22 @@
+
+import random
+
+
+class Message:
+	def __init__(self):
+		self.verbs = ["Generating", "Uploading", "Downloading", "Parsing",
+					"Heating", "Cooling", "Slicing", "Moulding", "Fixing",
+					"Inspecting", "Interrogating", "Shaking", "Shifting", 
+					"Reticulating", "Synthesizing", "Splurging", "BLASTing",
+					"Inserting", "Hand-crafting"]
+		self.nouns = ["data", "the cloud", "DNA", "nucleotides", "RAM",
+					"AI", "algorithms", "sequences", "pipettes", "00010111001",
+					"Python", "CSV", "files", "computer", "extensions", "URLs",
+					"domains", "IP addresses", "equations", "user-interface",
+					"FASTQs", "cores", "syntax", "bits and bytes"]
+
+
+	def random_message(self):
+		error = " ".join([random.choice(self.verbs), random.choice(self.nouns)])
+		return error
+
